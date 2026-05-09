e4ad797a5fdd079f0cca6be28022b8a162e3f847
Балобанов Владислав <vbalobanov@zunami.ru>
2026-05-09 09:11:13 +0300

    feat(auth interceptor): add parsing system tokens, refactor UserInfo->CallerInfo

--- grpc/interceptors/auth/interceptor.go
+++ grpc/interceptors/auth/interceptor.go
@@ -2,6 +2,7 @@ package authInterceptor
 
 import (
 	"context"
+	"strconv"
 	"strings"
 	"time"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/balobas/sport_city_common/logger"
 	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
 	uuid "github.com/satori/go.uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -42,61 +44,76 @@ func UnaryAuthInterceptor(withoutAuthMethods map[string]struct{}) grpc.UnaryServ
 			return nil, status.Error(codes.Unauthenticated, grpcErrors.AuthErrMsgTokenNotProvided)
 		}
 
-		authUserInfo, err := parseAndVerifyToken(ctx, accessJwt)
+		callerInfo, err := parseAndVerifyToken(ctx, accessJwt)
 		if err != nil {
 			return nil, status.Error(codes.Unauthenticated, err.Error())
 		}
 
-		log.Info().Msgf("user %s successfully verified", authUserInfo.UserUid)
+		log.Info().Msgf("user %s successfully verified", callerInfo.UserUid)
 
-		log = log.With().Str("callerUserUid", authUserInfo.UserUid.String()).Logger()
+		log = log.With().Str("callerUserUid", callerInfo.UserUid.String()).Logger()
 		ctx = logger.ContextWithLogger(ctx, log)
 
-		return handler(contextWithUserInfo(ctx, authUserInfo), req)
+		return handler(contextWithCallerInfo(ctx, callerInfo), req)
 	}
 }
 
-func parseAndVerifyToken(ctx context.Context, tokenStr string) (AuthUserInfo, error) {
+func parseAndVerifyToken(ctx context.Context, tokenStr string) (CallerInfo, error) {
 	log := logger.From(ctx)
 
 	token, _ := jwt.Parse(tokenStr, nil)
 	if token == nil {
 		log.Debug().Msg("auth interceptor: failed to parse jwt token: empty token")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
 	claims, _ := token.Claims.(jwt.MapClaims)
+
+	isSystem := false
+	isSystemVal, ok := claims["is_system"]
+	if ok {
+		isSystem, _ = strconv.ParseBool(isSystemVal.(string))
+	}
+
+	if isSystem {
+		return parseSystemToken(log, claims)
+	}
+
+	return parseUserToken(log, claims)
+}
+
+func parseUserToken(log zerolog.Logger, claims jwt.MapClaims) (CallerInfo, error) {
 	userUidStr, ok := claims["user_uid"]
 	if !ok {
 		log.Debug().Msg("auth interceptor: invalid jwt: user_uid is empty")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
 
 	userUid, err := uuid.FromString(userUidStr.(string))
 	if err != nil {
 		log.Debug().Msg("auth interceptor: invalid jwt: user_uid is invalid")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
 
 	deviceUidStr, ok := claims["device_uid"]
 	if !ok {
 		log.Debug().Msg("auth interceptor: invalid jwt: device_uid is empty")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
 
 	deviceUid, err := uuid.FromString(deviceUidStr.(string))
 	if err != nil {
 		log.Debug().Msg("auth interceptor: invalid jwt: device_uid is invalid")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
 
 	roles, ok := claims["roles"]
 	if !ok {
-		return AuthUserInfo{}, errors.New("empty roles in token")
+		return CallerInfo{}, errors.New("empty roles in token")
 	}
 
 	rolesStr, ok := roles.(string)
 	if !ok {
-		return AuthUserInfo{}, errors.New("invalid roles")
+		return CallerInfo{}, errors.New("invalid roles")
 	}
 	var rolesStrs []string
 	if len(rolesStr) != 0 {
@@ -106,44 +123,123 @@ func parseAndVerifyToken(ctx context.Context, tokenStr string) (AuthUserInfo, er
 	expiredStr, ok := claims["expired_at"]
 	if !ok {
 		log.Debug().Msg("auth interceptor: invalid jwt: expired is empty")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
 
 	expiredAt, ok := expiredStr.(float64)
 	if !ok {
 		log.Debug().Msg("auth interceptor: invalid jwt: failed to parse expired")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
 
 	if int64(expiredAt) <= time.Now().UTC().Unix() {
 		log.Debug().Msg("auth interceptor: invalid jwt: token expired")
-		return AuthUserInfo{}, errors.New(grpcErrors.AuthErrMsgTokenExpired)
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgTokenExpired)
 	}
 
-	userInfo := AuthUserInfo{
+	return CallerInfo{
 		UserUid:   userUid,
 		DeviceUid: deviceUid,
 		Roles:     rolesStrs,
+		IsSystem:  false,
+	}, nil
+}
+
+func parseSystemToken(log zerolog.Logger, claims jwt.MapClaims) (CallerInfo, error) {
+	serviceUidStr, ok := claims["service_uid"]
+	if !ok {
+		log.Debug().Msg("auth interceptor: invalid jwt: service_uid is empty")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
 	}
-	return userInfo, nil
+
+	serviceUid, err := uuid.FromString(serviceUidStr.(string))
+	if err != nil {
+		log.Debug().Msg("auth interceptor: invalid jwt: service_uid is invalid")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+	}
+
+	deviceUidStr, ok := claims["device_uid"]
+	if !ok {
+		log.Debug().Msg("auth interceptor: invalid jwt: device_uid is empty")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+	}
+
+	deviceUid, err := uuid.FromString(deviceUidStr.(string))
+	if err != nil {
+		log.Debug().Msg("auth interceptor: invalid jwt: device_uid is invalid")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+	}
+
+	domainVal, ok := claims["domain"]
+	if !ok {
+		log.Debug().Msg("auth interceptor: invalid jwt: domain is empty")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+	}
+
+	domain, ok := domainVal.(string)
+	if !ok {
+		log.Debug().Msg("auth interceptor: invalid jwt: domain is invalid")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+	}
+
+	roles, ok := claims["roles"]
+	if !ok {
+		return CallerInfo{}, errors.New("empty roles in token")
+	}
+
+	rolesStr, ok := roles.(string)
+	if !ok {
+		return CallerInfo{}, errors.New("invalid roles")
+	}
+	var rolesStrs []string
+	if len(rolesStr) != 0 {
+		rolesStrs = strings.Split(rolesStr, "")
+	}
+
+	expiredStr, ok := claims["expired_at"]
+	if !ok {
+		log.Debug().Msg("auth interceptor: invalid jwt: expired is empty")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+	}
+
+	expiredAt, ok := expiredStr.(float64)
+	if !ok {
+		log.Debug().Msg("auth interceptor: invalid jwt: failed to parse expired")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgInvalidToken)
+	}
+
+	if int64(expiredAt) <= time.Now().UTC().Unix() {
+		log.Debug().Msg("auth interceptor: invalid jwt: token expired")
+		return CallerInfo{}, errors.New(grpcErrors.AuthErrMsgTokenExpired)
+	}
+
+	return CallerInfo{
+		UserUid:   serviceUid,
+		DeviceUid: deviceUid,
+		Roles:     rolesStrs,
+		IsSystem:  true,
+		Domain:    domain,
+	}, nil
 }
 
 type userCtxKey struct{}
 
-func contextWithUserInfo(ctx context.Context, info AuthUserInfo) context.Context {
+func contextWithCallerInfo(ctx context.Context, info CallerInfo) context.Context {
 	return context.WithValue(ctx, userCtxKey{}, info)
 }
 
-func UserInfoFromContext(ctx context.Context) AuthUserInfo {
-	info, ok := ctx.Value(userCtxKey{}).(AuthUserInfo)
+func CallerInfoFromContext(ctx context.Context) CallerInfo {
+	info, ok := ctx.Value(userCtxKey{}).(CallerInfo)
 	if !ok {
-		return AuthUserInfo{}
+		return CallerInfo{}
 	}
 	return info
 }
 
-type AuthUserInfo struct {
+type CallerInfo struct {
 	UserUid   uuid.UUID
 	DeviceUid uuid.UUID
 	Roles     []string
+	IsSystem  bool
+	Domain    string
 }
